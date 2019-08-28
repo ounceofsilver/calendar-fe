99aa1f76ccfafe8e114236901571116405332b39
James Fulford <james.fulford@outlook.com>
2019-08-28 00:08:07 -0400

    feat(api): plan

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,3 +8,51 @@ function getToken() {
   }
   return Promise.reject({ error: "user.not-logged-in" });
 }
+
+var setPlan = firebase.functions().httpsCallable("onboarding");
+
+function startTrial () {
+  var result = setPlan({
+    tier: "BASIC",
+    strategy: "MONTHLY",
+    isTrial: true
+  });
+  result.then(console.log);
+  debugger;
+}
+
+function setBasicMonthly () {
+  var result = setPlan({
+    tier: 'BASIC',
+    strategy: 'MONTHLY',
+    isTrial: false,
+  });
+  debugger;
+}
+
+function setBasicYearly() {
+  var result = setPlan({
+    tier: "BASIC",
+    strategy: "YEARLY",
+    isTrial: false
+  });
+  debugger;
+}
+
+function setAdvancedMonthly() {
+  var result = setPlan({
+    tier: "ADVANCED",
+    strategy: "MONTHLY",
+    isTrial: false
+  });
+  debugger;
+}
+
+function setAdvancedMonthly() {
+  var result = setPlan({
+    tier: "ADVANCED",
+    strategy: "YEARLY",
+    isTrial: false
+  });
+  debugger;
+}
